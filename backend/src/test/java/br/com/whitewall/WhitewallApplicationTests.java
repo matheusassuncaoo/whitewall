df488df553968061f26e78ca75b94d9f7db2a300
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2026-06-18 08:51:58 -0400

    chore: initial project structure

---
+++ backend/src/test/java/br/com/whitewall/WhitewallApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.whitewall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitewallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
